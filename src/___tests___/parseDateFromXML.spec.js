dd99c46abd3d843d210e53a0d9c118cbc6c7f6fa
mwalker <mwalker@sigmaequipment.com>
2023-05-08 16:52:54 -0500

    -desc The Dates from the XML file where not in a valid JavScript date format, Added a parser ,so they could be added to the database -fixes -changes -additions

---
+++ src/___tests___/parseDateFromXML.spec.js
@@ -0,0 +1,33 @@
+import { parseDateFromXML } from "../modules/parseDateFromXML";
+import {describe,it,expect} from "vitest"
+
+
+describe('parseDate', () => {
+    it('should parse a valid date string', () => {
+        const input = '08/05/2023 16:30:45';
+        const expected = new Date('2023-05-08T16:30:45');
+        const result = parseDateFromXML(input);
+        expect(result).toEqual(expected);
+    });
+
+    it('should handle single-digit day and month values', () => {
+        const input = '01/01/2023 01:01:01';
+        const expected = new Date('2023-01-01T01:01:01');
+        const result = parseDateFromXML(input);
+        expect(result).toEqual(expected);
+    });
+
+    it('should handle leading/trailing whitespace', () => {
+        const input = '   08/05/2023 16:30:45   ';
+        const expected = new Date('2023-05-08T16:30:45');
+        const result = parseDateFromXML(input);
+        expect(result).toEqual(expected);
+    });
+
+    it('should throw an error for invalid input', () => {
+        const input = 'invalid date';
+        expect(() => {
+            parseDateFromXML(input);
+        }).toThrow();
+    });
+});
---
+++ src/modules/parseDateFromXML.ts
@@ -0,0 +1,17 @@
+export function parseDateFromXML(dateTime:string) {
+    try {
+        dateTime = dateTime.trim();
+        const [date, time] = dateTime.split(" ");
+        const [DD, MM, YYYY] = date.split("/");
+        const [h, m, s] = time.split(":");
+        let output =  new Date(+YYYY, +MM - 1, +DD, +h, +m, +s);
+        if(output.toString() === "Invalid Date"){
+            throw new Error("Invalid date format");
+        }
+        return output;
+    }
+    catch (e) {
+        // should throw error invalid date format
+        throw new Error("Invalid date format");
+    }
+}
