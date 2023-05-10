943fd70fa21c0063af13767bd9cb215c952d3273
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-05-10 14:20:48 -0500

    -desc removed unused serial field -fixes -changes code style changes -additions

--- src/scripts/WipeHardDrive.sh
+++ src/scripts/WipeHardDrive.sh
@@ -6,18 +6,16 @@ long_term_storage_path="/Volumes/LongTermStorage"
 api_url="https://api.example.com"
 
 # Prompt user for serial number and company name
-read -r -p "Enter serial number: " serial_number
 read -r -p "Enter company name: " company_name
-
 #killdisk
 
 for file in "$temp_folder_path"/*; do
       # Upload file to API
       # this is how the file gets to my server
-      curl -X POST -F "file=@$file" "$api_url/api/addHardDrive?serial_number=$serial_number&company_name=$company_name&name=$file"
+      curl -X POST -F "file=@$file" "$api_url/api/addHardDrive?company_name=$company_name&name=$file"
       # if this is doug reading this is where you would put the code to move the file to long term storage
       # and rename the file however you want
       extension="${file##*.}"
-      new_filename="${serial_number}_${company_name}_.$extension"
+      new_filename="${company_name}_.$extension"
       mv "$file" "$long_term_storage_path/$new_filename"
 done
