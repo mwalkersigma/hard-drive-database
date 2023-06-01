854062ac729948c896b5bc26f95c99c51200fe54
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2023-06-01 08:50:58 -0500

    -desc Greatly improved reject speed for duplicate files -fixes -changes -additions

--- src/modules/validateRequest.ts
+++ src/modules/validateRequest.ts
@@ -1,10 +1,9 @@
 import {NextApiRequest} from "next";
 
-export default function validateRequest(req:NextApiRequest,previouslyParsed:any[]):{valid:boolean,message:string}{
+export default function validateRequest(req:NextApiRequest):{valid:boolean,message:string}{
     let badRequest = (message:string) => ({valid:false,message});
     if(req.method !== "POST") return badRequest("Please Make Sure To Only Send POST Requests To This Route");
     if(!req.query?.name)return badRequest("Post Requests To This Route MUST Include A file Name");
     if(!req.query?.company_name)return badRequest("Post Requests To This Route MUST Include A company_name");
-    if(previouslyParsed.includes(req.query.name))return badRequest("This file has already been added to DB")
     return {valid:true,message:""};
 }
--- src/pages/api/addHardDrive.ts
+++ src/pages/api/addHardDrive.ts
@@ -6,49 +6,38 @@ import handleSingleReport from "../../modules/xmlFileHandler/reportHandler";
 import handleBatchReport from "../../modules/xmlFileHandler/batchHandler";
 import getDateString from "../../modules/getDateString";
 
-// get the day from date string
-let currentDay = "";
-let getCurrentDay = ()=>getDateString().split(" ")[1].split(",")[0];
-let fileNames:any[] = []
 interface Handlers {
     [key:string]:(parsedFile:any,log:any)=>any
 }
-async function getFileNames(){
-    if(getCurrentDay() === currentDay){
-        return fileNames;
-    }
-    let filenameRows = await db.query(`SELECT file_name FROM report;`);
-    fileNames = filenameRows.rows.map((row:any)=>row.file_name);
-    currentDay = getCurrentDay();
-    return fileNames;
+async function checkIfFileHasBeenParsed(name:string){
+    let hasName = await db.query(`SELECT file_name FROM report WHERE file_name = $1`,[name])
+    return hasName.rows.length !== 0
 }
 function determineReportType(file:any){
     return (file.report?.bays || file.report.name.includes("batch")) ? "batch" : "single"
 }
 
-
 export default function handler (req:any,res:any) {
     let responder = handleRes(res);
     db.logger.log(`request started at : ${getDateString()}`);
-    return getFileNames()
-            .then((filenames)=>validateRequest(req,filenames))
-            .then(({valid,message})=> {
-                if(valid) {
-                    const parsedReportFile: any = parseXMLFile(req.body);
-                    parsedReportFile.report.name = req.query.name;
-                    parsedReportFile.report.company = req.query.name;
-                    const reportType = determineReportType(parsedReportFile);
-                    let handlers: Handlers = {single: handleSingleReport, batch: handleBatchReport};
-                    db.logger.log(`Handling ${reportType} report`);
-                    return handlers[reportType](parsedReportFile, db.logger.log)
-                }
-                return Promise.reject(" File Already Parsed")
-
-            })
-            .then(()=>{
-                db.logger.log(`File Successfully parsed : ${req.query.name}}`);
-                fileNames.push(req.query.name)
-                responder(200,"File Successfully parsed");
-            })
-            .catch(()=>responder(413,"File has already been parsed"))
+    return checkIfFileHasBeenParsed(req.query.name)
+        .then((hasBeenParsed:boolean)=>hasBeenParsed && Promise.reject(" File Already Parsed"))
+        .then(()=>validateRequest(req))
+        .then(({valid,message})=> {
+            if(valid) {
+                const parsedReportFile: any = parseXMLFile(req.body);
+                parsedReportFile.report.name = req.query.name;
+                parsedReportFile.report.company = req.query.name;
+                const reportType = determineReportType(parsedReportFile);
+                let handlers: Handlers = {single: handleSingleReport, batch: handleBatchReport};
+                db.logger.log(`Handling ${reportType} report`);
+                return handlers[reportType](parsedReportFile, db.logger.log)
+            }
+            return Promise.reject(message)
+        })
+        .then(()=>{
+            db.logger.log(`File Successfully parsed : ${req.query.name}}`);
+            responder(200,"File Successfully parsed");
+        })
+        .catch((msg)=>responder(413,msg))
 }
